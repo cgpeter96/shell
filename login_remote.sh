47b1c342e48cc03731d89bc3b66a99d9469c1be4
Guang Chen <cgpeter96@users.noreply.github.com>
2022-04-27 19:28:51 +0800

    Update login_remote.sh
    形成统一登录脚本

--- login_remote.sh
+++ login_remote.sh
@@ -1,34 +1,64 @@
 #!/bin/bash
-# 用于快速登录机器,免于一直输入密码
-# 基于expect，如果没有expect需要安装下
-username=""
-password=""
+# 用于管理快速登录服务器，减少显示输入密码
+# 依赖：expect,这个工具需要提前安装 yum install expect or apt install expect
+default_username=""
+default_password=""
+default_port=22
+msg_list=()
 host_list=()
+username_list=()
+password_list=()
 port_list=()
 
-#此处写相应的机器host和port 如果机器密码不一样需要创建下相应的password_list
-host_list[${#host_list[@]}]="wwwww"
-port_list[${#port_list[@]}]=22
 
+#instruction of connection for remote server
+host_list[${#host_list[@]}]=""
+username_list[${#username_list[@]}]=""
+password_list[${password_list[@]}]=""
+port_list[${#port_list[@]}]=""
+msg_list[${#msg_list[@]}]=""
 
+#e.g
+#host_list[${#host_list[@]}]="xxx.xx.com"
+#username_list[${#username_list[@]}]="username"
+#password_list[${password_list[@]}]="password"
+#port_list[${#port_list[@]}]="22"
+#msg_list[${#msg_list[@]}]="测试机"
+
+#print the server list
+fmt="%-5s\t%-30s\t%-5s\t%-30s\n"
+printf $fmt "index" "hostname" "port" "message"
 for((i=0;i<${#host_list[@]};i++))
 do
-    echo "$i:  ${host_list[$i]} "
+    printf $fmt $i ${host_list[$i]}  ${port_list[$i]} ${msg_list[$i]}
 done
 
 choose_idx=0
-read -p "选择服务器:" arg
+read -p "Select Server:>>>" arg
 if [ "$arg" -ge 0 ];then
    choose_idx=$arg
+else
+   echo "please check input:$arg"
+   exit 1
 fi
 
 
 ip=${host_list[$choose_idx]}
 port=${port_list[$choose_idx]}
-echo "${username}@${ip}:$port"
+username=${username_list[$choose_idx]}
+password=${password_list[$choose_idx]}
+# check server
+if [[ ! -n $ip || ! -n $port || ! -n $username || ! -n $password ]]
+then
+    echo -e "${ip}\t${port}\t${username}\t${password}"
+    echo "ERROR:please check server information"
+    exit 1
+fi
+echo "Connect to ${username}@${ip}:$port"
 
-expect -c "spawn ssh -p $port  $username@$ip
-# note login .. 这行用于匹配无需输入密码的，但是提前登录后需要有login相关的提示，才能工作。
+# build connection
+#exp_continue匹配到会往下执行
+expect -c "spawn ssh -p $port $username@$ip
 expect {
 \"*login*\" { interact }
 \"*yes/no*\" { send \"yes\r\";exp_continue }
